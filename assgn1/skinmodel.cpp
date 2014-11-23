7a4e3b21739bf7eba721a585e53ed994034d2f03
Matthias Holoch <mholoch@gmail.com>
2014-11-23 17:06:25 +0100

    implemented a skin probability map using the ycrcb color space (actually just using the cr cb components.

--- assgn1/skinmodel.cpp
+++ assgn1/skinmodel.cpp
@@ -2,16 +2,45 @@
 #include <cmath>
 #include <iostream>
 
-using namespace std;
+#define voodoo ((SkinProbMap*) pimpl)
+
+class SkinProbMap {
+  public:
+    SkinProbMap() {
+      map = new cv::Mat1d(256, 256);
+      total_skin = 0;
+    }
+
+    ~SkinProbMap(){}
+
+    double add_training_skin_pixel(cv::Vec3b ycrcb) {
+      total_skin++;
+      map->at<double>(ycrcb[1], ycrcb[2]) += 1;
+
+      return map->at<double>(ycrcb[1], ycrcb[2]);
+    }
+
+    float test_skin_pixel(cv::Vec3b ycrcb) {
+       double div = map->at<double>(ycrcb[1], ycrcb[2]) / total_skin;
+       return div;
+    }
+
+  private:
+    cv::Mat1d* map;
+    double total_skin;
+};
 
 /// Constructor
 SkinModel::SkinModel()
 {
+  SkinProbMap* sm = new SkinProbMap();
+  pimpl = (SkinModelPimpl*) sm;
 }
 
 /// Destructor
 SkinModel::~SkinModel() 
 {
+  delete voodoo;
 }
 
 /// Start the training.  This resets/initializes the model.
@@ -20,7 +49,6 @@ SkinModel::~SkinModel()
 /// Use this function to initialize/clear data structures used for training the skin model.
 void SkinModel::startTraining()
 {
-    //--- IMPLEMENT THIS ---//
 }
 
 /// Add a new training image/mask pair.  The mask should
@@ -30,7 +58,16 @@ void SkinModel::startTraining()
 /// @param mask: mask which specifies, which pixels are skin/non-skin
 void SkinModel::train(const cv::Mat3b& img, const cv::Mat1b& mask)
 {
-	//--- IMPLEMENT THIS ---//
+  cv::Mat3b img_ycrcb;
+  cv::cvtColor(img, img_ycrcb, CV_BGR2YCrCb);
+  for (int row = 0; row < img.rows; ++row) {
+    for (int col = 0; col < img.cols; ++col) {
+      if (mask(row,col) == 255) {
+        double total_skin = voodoo->add_training_skin_pixel(img_ycrcb(row,col));
+        //std::cout << "Adding skin pixel at [" << (int)img_ycrcb(row,col)[1] << "," << (int)img_ycrcb(row,col)[2]  << "] , there were " << total_skin << "skin pixels with the same CrCb value." << std::endl;
+      }
+    }
+  }
 }
 
 /// Finish the training.  This finalizes the model.  Do not call
@@ -40,7 +77,6 @@ void SkinModel::train(const cv::Mat3b& img, const cv::Mat1b& mask)
 /// e.g normalize w.r.t. the number of training images etc.
 void SkinModel::finishTraining()
 {
-	//--- IMPLEMENT THIS ---//
 }
 
 
@@ -52,24 +88,17 @@ void SkinModel::finishTraining()
 cv::Mat1b SkinModel::classify(const cv::Mat3b& img)
 {
     cv::Mat1b skin = cv::Mat1b::zeros(img.rows, img.cols);
+    cv::Mat3b img_ycrcb;
+    cv::cvtColor(img, img_ycrcb, CV_BGR2YCrCb);
 
-	//--- IMPLEMENT THIS ---//
     for (int row = 0; row < img.rows; ++row) {
-        for (int col = 0; col < img.cols; ++col) {
-
-			if (false)
-				skin(row, col) = rand()%256;
-
-			if (false)
-				skin(row, col) = img(row,col)[2];
-
-			if (true) {
-			
-				cv::Vec3b bgr = img(row, col);
-				if (bgr[2] > bgr[1] && bgr[1] > bgr[0]) 
-					skin(row, col) = 2*(bgr[2] - bgr[0]);
-			}
+      for (int col = 0; col < img.cols; ++col) {
+        double skin_prob = voodoo->test_skin_pixel(img_ycrcb(row,col));
+        //std::cout << "Probability for skin at [" << (int)img_ycrcb(row,col)[1] << "," << (int)img_ycrcb(row,col)[2] << "] is " << skin_prob << std::endl;
+        if (skin_prob > 0.004) {
+          skin(row,col) = 255;
         }
+			}
     }
 
     return skin;
